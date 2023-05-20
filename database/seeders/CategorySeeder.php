6fc4fd4b426ba377e899c79287414d635c9b979a
tomoki <tmdesign.0223@gmail.com>
2023-05-20 15:26:20 +0900

    CategorySeederのcount変更

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,6 +14,6 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        Category::factory()->count(3)->create();
+        Category::factory()->count(2)->create();
     }
 }
