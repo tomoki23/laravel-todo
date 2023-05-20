f590627b370b7f76b43e9f5aabad6ca355dd6dce
tomoki <tmdesign.0223@gmail.com>
2023-05-21 03:43:05 +0900

    task登録時のバリデーション作成

---
+++ app/Http/Requests/CreateTaskRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|integer',
+            'title' => 'required|min:0|max:10|string',
+            'body' => 'required|min:0|max:30|string',
+            'category_id' => 'required'
+        ];
+    }
+}
