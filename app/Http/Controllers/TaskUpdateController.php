cb7fa5c86c64c6d12dcc4eaf870686c229674e93
tomoki <tmdesign.0223@gmail.com>
2023-05-21 03:48:32 +0900

    update時のバリーデーションルール作成

--- app/Http/Controllers/TaskUpdateController.php
+++ app/Http/Controllers/TaskUpdateController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
 use Symfony\Component\Console\Input\Input;
 
 class TaskUpdateController extends Controller
 {
-    public function update(Request $request, $id)
+    public function update(UpdateTaskRequest $request, $id)
     {
         $task = Task::find($id);
         $task->title = $request->input('title');
