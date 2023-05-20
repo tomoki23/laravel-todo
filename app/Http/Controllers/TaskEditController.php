4fdc8205b8b88e98944049ccc6d5f5be2dcf2779
tomoki <tmdesign.0223@gmail.com>
2023-05-20 15:24:30 +0900

    TaskEdit作成

---
+++ app/Http/Controllers/TaskEditController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Task;
+
+class TaskEditController extends Controller
+{
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        $categories = Category::all();
+
+        return view('tasks.edit', compact('task', 'categories'));
+    }
+}
