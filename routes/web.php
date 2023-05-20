d9c9f8ebe9f31da0a62e964147ea7fd25bd4c77d
tomoki <tmdesign.0223@gmail.com>
2023-05-21 08:45:41 +0900

    ルート追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskIndexController;
+use App\Http\Controllers\TaskCreateController;
+use App\Http\Controllers\TaskStoreController;
+use App\Http\Controllers\TaskShowController;
+use App\Http\Controllers\TaskEditController;
+use App\Http\Controllers\TaskUpdateController;
+use App\Http\Controllers\TaskDeleteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +33,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/tasks', [TaskIndexController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskCreateController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskStoreController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{id}', [TaskShowController::class, 'show'])->name('tasks.show');
+    Route::get('/tasks/{id}/edit', [TaskEditController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{id}/update', [TaskUpdateController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{id}/delete', [TaskDeleteController::class, 'destroy'])->name('tasks.delete');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
