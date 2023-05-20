9ca8906db733fedd803c8ce1ab918324733e8c0d
tomoki <tmdesign.0223@gmail.com>
2023-05-21 03:43:52 +0900

    登録画面にバリデーション時のエラーメッセージ追加

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,19 +4,28 @@
       @csrf
       <div class="mb-4">
         <label for="title">タイトル</label>
-        <select name="category" class="w-32">
-          <option>カテゴリー</option>
+        <select name="category_id" class="w-32">
+          <option value="">カテゴリー</option>
           @foreach ($categories as $category)
-            <option name="{{ $category->name }}" value="{{ $category->id }}">{{ $category->name }}</option>
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
           @endforeach
         </select>
+        @error('category_id')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
       <div class="mb-4">
         <input type="text" name="title" id="title" class="w-full">
+        @error('title')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
       <div class="mb-4">
         <label for="body">本文</label>
         <input type="text" name="body" id="body" class="w-full">
+        @error('body')
+          <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
       </div>
       <div class="flex justify-center">
         <x-primary-button>登録</x-primary-button>
