74a01b2ca320ea6851d66b30b9bac6cfb70c2e26
tomoki <tmdesign.0223@gmail.com>
2023-05-20 15:27:10 +0900

    更新画面変更

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,20 +1,23 @@
 <x-app-layout>
   <div class="flex flex-col items-center">
-    <form action="#" class="max-w-lg mx-auto mt-6 p-6 bg-white shadow-md rounded-md">
+    <form action="{{ route('tasks.update', ['id' => $task->id]) }}" method="POST" class="max-w-lg mx-auto mt-6 p-6 bg-white shadow-md rounded-md">
+      @csrf
+      @method('PUT')
       <div class="mb-4">
         <label for="title">タイトル</label>
         <select name="category" class="w-32">
           <option>カテゴリー</option>
-          <option value="カテゴリー">カテゴリー</option>
-          <option value="カテゴリー">カテゴリー</option>
+          @foreach ($categories as $category)
+          <option name="{{ $category->name }}" value="{{ $category->id }}"{{ $category->name == $task->category->name ? 'selected' : '' }}>{{ $category->name }}</option>
+          @endforeach
         </select>
       </div>
       <div class="mb-4">
-        <input type="text" name="title" id="title" class="w-full">
+        <input type="text" name="title" id="title" class="w-full" value="{{ $task->title }}">
       </div>
       <div class="mb-4">
         <label for="body">本文</label>
-        <input type="text" name="body" id="body" class="w-full">
+        <input type="text" name="body" id="body" class="w-full" value="{{ $task->body }}">
       </div>
       <div class="flex justify-center">
         <x-primary-button>更新</x-primary-button>
